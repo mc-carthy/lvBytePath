f1311b7470208ccfdcf430f16795237123fba522
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-05-10 22:21:02 +0100

    Reinstate director

--- src/objects/director.lua
+++ src/objects/director.lua
@@ -20,19 +20,22 @@ function Director:new(stage)
     }
     self.enemySpawnChance = {
         [1] = ChanceList({ 'Rock', 1 }),
-        [2] = ChanceList({ 'Rock', 8 }, { 'Shooter', 4 }),
-        [3] = ChanceList({ 'Rock', 8 }, { 'Shooter', 8 }),
-        [4] = ChanceList({ 'Rock', 4 }, { 'Shooter', 8 }),
+        [2] = ChanceList({ 'Rock', 1 }),
+        [3] = ChanceList({ 'Rock', 1 }),
+        [4] = ChanceList({ 'Rock', 1 }),
+        -- [2] = ChanceList({ 'Rock', 8 }, { 'Shooter', 4 }),
+        -- [3] = ChanceList({ 'Rock', 8 }, { 'Shooter', 8 }),
+        -- [4] = ChanceList({ 'Rock', 4 }, { 'Shooter', 8 }),
     }
     for i = 5, 1024 do
         self.enemySpawnChance[i] = ChanceList(
-      	    { 'Rock', love.math.random(2, 12) }, 
-      	    { 'Shooter', love.math.random(2, 12) }
+      	    { 'Rock', love.math.random(2, 12) }
+      	    -- { 'Shooter', love.math.random(2, 12) }
     	)
     end
 
     self:setEnemySpawnForThisRound()
-    self.stage.area:addGameObject(Attack)
+    self.stage.area:addGameObject('Attack', Random(0, gw), Random(0, gh)) 
 end
 
 function Director:setEnemySpawnForThisRound()
@@ -59,6 +62,7 @@ function Director:setEnemySpawnForThisRound()
 end
 
 function Director:update(dt)
+    self.timer:update(dt)
     self.roundTimer = self.roundTimer + dt
     if self.roundTimer > self.roundTime then
         self.difficulty = self.difficulty + 1
